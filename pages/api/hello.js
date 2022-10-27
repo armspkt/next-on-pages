64330edf1a57fdeef7be9e9ea7e50987d6beb560
Arm Supakorn <arm.supakorn@bitkub.com>
2022-10-27 14:08:27 +0700

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default async function handler(req, res) {
+  return new Response(JSON.stringify({ name: "John Doe" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
